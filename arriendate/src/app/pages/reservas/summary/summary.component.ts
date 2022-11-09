1f09ca3936fee51edb7daef368879f7e3277a21c
valdivieso.ant <valdivieso.ant@gmail.com>
2022-11-09 19:22:32 -0300

    Modificacion arriendos vigentes

--- arriendate/src/app/pages/reservas/summary/summary.component.ts
+++ arriendate/src/app/pages/reservas/summary/summary.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DOCUMENT, Location } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
@@ -11,7 +13,7 @@ export class SummaryComponent implements OnInit {
   id_reserva:any;
   reserva_detail:any;
 
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService, private acroute: ActivatedRoute, private location : Location, private router:Router) { }
 
   ngOnInit(): void {
     this.id_reserva = localStorage.getItem('datos_reserva');
@@ -22,11 +24,15 @@ export class SummaryComponent implements OnInit {
   getReserveById(){
     this.api.getReserveByID(this.id_reserva).subscribe({
       next:(res)=>{
-        console.log(res);
         this.reserva_detail = res
-         
+        console.log(this.reserva_detail[0]);
+        
       }
     })
+  }
+
+  goBack(){
+    this.router.navigate(['home'])
   }
 
 
